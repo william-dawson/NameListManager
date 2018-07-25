5ca29eea0d04f9d10013c5ad676cfe91cd507de3
William Dawson <william.dawson@riken.jp>
2018-07-25 10:17:47 +0900

    Packaging prep

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-# NameListManager
+# Name List Manager
 
 This program automatically generates code and documentation for Fortran
-programs. To use it, you first write an xml file which describes all the
-input parameters. An example input file is available in `example/input.xml` .
-This xml file describes all the parameters, as well as their data types
-and purposes. Then, you run this program on the xml file, and it automatically
-generates:
+programs that manage their input using namelists. To use it, you first write an
+xml file which describes all the input parameters. An example input file is
+available in `example/input.xml` . This xml file describes all the parameters,
+as well as their data types and purposes. Then, you run this program on the
+xml file, and it automatically generates:
 
 -   Restructered text documenting the parameters, including default values.
 -   Module files to store all the input variables.
--   Reader subroutines that read in data from a namelist specifying input values.
+-   Reader subroutines that read in data from a namelist specifying input
+    values.
 
 You run the program as:
 
---
+++ namelistmanager/common.py
@@ -0,0 +1,56 @@
+'''
+This file contains the code needed to generate the common module used to
+support the readers.
+'''
+
+
+def create_common(output_path):
+    '''
+    Creates the common module.
+
+    output_path: pass the path to the modules.
+    '''
+    file_name = output_path + "/io_common_module.f90"
+
+    try:
+        ofile = open(file_name, "w")
+    except:
+        print("Couldn't open file:", file_name)
+        quit()
+
+    # Header
+    off = " " * 6
+    offcom = " " * 5
+    # write_comment(ofile, module.find("description").text, offcom)
+    ofile.write("!" + offcom + "Common information to support the readers\n")
+    ofile.write(off + "MODULE IOCommonModule\n")
+    ofile.write("!\n")
+    ofile.write(off + "IMPLICIT NONE\n")
+    ofile.write("!\n")
+
+    # Common members
+    ofile.write(off + "INTEGER, PARAMETER :: fname_len = 80\n")
+    ofile.write(off + "INTEGER, PARAMETER :: instr_len = 20\n")
+    ofile.write("!\n")
+
+    # Subroutines
+    ofile.write(off + "CONTAINS\n")
+    ofile.write("!\n")
+
+    # Error Handler
+    ofile.write("!" + offcom + "If there is an I/O error, call this routine.\n")
+    ofile.write(off + "SUBROUTINE HandleError(fname)\n")
+    ofile.write("!\n")
+    ofile.write(off + "CHARACTER(len=fname_len), INTENT(IN) :: fname\n")
+    ofile.write("!\n")
+    ofile.write(off + "WRITE(*,*) \"Problem with file \", fname\n")
+    ofile.write(off + "EXIT\n")
+    ofile.write("!\n")
+    ofile.write(off + "END SUBROUTINE HandleError\n")
+
+    # Footer
+    ofile.write("!\n")
+    ofile.write(off + "END MODULE")
+
+    # Cleanup
+    ofile.close()
